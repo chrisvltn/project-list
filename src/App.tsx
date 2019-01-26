21d394fea74713f3d43e801d44eed3891a6b2259
chrisvltn <chris_valentin@outlook.com>
2019-01-26 13:15:41 -0200

    Adds routes to the created pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import ProjectForm from './routes/ProjectListing/Create/ProjectForm';
+import ProjectList from './routes/ProjectListing/List/ProjectList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="flex justify-center">
+				<div className="pv4 ph3 mw6 w-100">
+					<Link to="/project">List</Link>
+					<Link to="/project/create">Create</Link>
+
+					<Switch>
+						<Route path="/project/create" exact component={ProjectForm} />
+						<Route path="/project/" component={ProjectList} />
+						<Redirect path="/" to="/project" />
+					</Switch>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
