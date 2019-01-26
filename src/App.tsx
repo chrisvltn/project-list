20589e24289ff2388f0504c6765569656d1f4ede
chrisvltn <chris_valentin@outlook.com>
2019-01-26 15:41:09 -0200

    Styles navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { connect, MapDispatchToPropsFunction } from 'react-redux';
 import { Project } from './entities/Project';
 import { add } from './store/actions';
 import { list } from './services/ProjectService';
+import Navigation from './components/Navigation/Navigation';
 
 type MappedProps = { add: (project: Project[]) => void }
 type Props = RouteComponentProps & MappedProps
@@ -20,9 +21,9 @@ class App extends Component<Props> {
 	render() {
 		return (
 			<div className="flex justify-center">
-				<div className="pv4 ph3 mw6 w-100">
-					<Link to="/project">List</Link>
-					<Link to="/project/create">Create</Link>
+				<div className="pt1 pb4 ph3 mw6 w-100">
+					<h1 className="f1 lh-title tc">Project List</h1>
+					<Navigation />
 
 					<Switch>
 						<Route path="/project/create" exact component={ProjectForm} />
