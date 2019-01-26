c16bef25ecd3f10f7de4adbb621200230dad6b47
Christian Valentin <chris_valentin@outlook.com>
2019-01-26 15:25:22 -0200

    Implement redux in the components

---
+++ src/components/UI/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React, { HTMLProps, JSXElementConstructor } from 'react'
+
+type Props = HTMLProps<HTMLButtonElement> & {
+	btnType: 'success' | 'danger'
+	small?: boolean
+	clear?: boolean
+}
+
+const Button: JSXElementConstructor<Props> = ({
+	btnType,
+	small,
+	clear,
+	children,
+	className,
+	...props
+}) => {
+	const buttonClass = ['db w-100 ttu bn b tc pointer dim']
+
+	if (className)
+		buttonClass.push(className)
+
+	buttonClass.push(small ? 'ph1 pv2 f6' : 'ph2 pv3 f5')
+
+	switch (btnType) {
+		case 'success':
+			buttonClass.push(clear ? 'bg-transparent green' : 'bg-green white')
+			break
+		case 'danger':
+			buttonClass.push(clear ? 'bg-transparent red' : 'bg-red white')
+			break
+	}
+
+	return (
+		<button className={buttonClass.join(' ')} {...props}>
+			{children}
+		</button>
+	)
+}
+
+export default Button
