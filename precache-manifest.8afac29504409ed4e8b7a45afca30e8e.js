2cdf366e434d3a36d16d75934ade00c2204e99e6
Christian Valentin <chris_valentin@outlook.com>
2019-01-26 16:03:19 -0200

    Updates

---
+++ precache-manifest.8afac29504409ed4e8b7a45afca30e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cccf063aec313413a90",
+    "url": "/project-list/static/js/runtime~main.7cccf063.js"
+  },
+  {
+    "revision": "ec8492c13dc8e8d20722",
+    "url": "/project-list/static/js/main.ec8492c1.chunk.js"
+  },
+  {
+    "revision": "732f905875bd44e8fc4d",
+    "url": "/project-list/static/js/1.732f9058.chunk.js"
+  },
+  {
+    "revision": "ec8492c13dc8e8d20722",
+    "url": "/project-list/static/css/main.3fff3842.chunk.css"
+  },
+  {
+    "revision": "fa10875f0bfc7565b16d03d2c493fe2b",
+    "url": "/project-list/index.html"
+  }
+];
