3b2efa04780da53bfeaab336a46047fd032ac56d
Christian Valentin <chris_valentin@outlook.com>
2019-01-26 15:57:27 -0200

    Apply HashRouter and ServiceWorker

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store/store'
 
+const basename = process.env.NODE_ENV === 'production' ? '/project-list' : '/'
+
 const app =
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter basename={basename}>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 
 ReactDOM.render(app, document.getElementById('root'));
@@ -19,4 +21,4 @@ ReactDOM.render(app, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
