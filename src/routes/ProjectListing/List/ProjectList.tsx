269a7bdabbf0cf10ed343e8fd0ad06fabeec99c2
chrisvltn <chris_valentin@outlook.com>
2019-01-26 13:15:19 -0200

    Creates mocked Project Listing page

---
+++ src/routes/ProjectListing/List/ProjectList.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ProjectItem from '../../../components/ProjectItem/ProjectItem';
+
+class ProjectList extends Component {
+	render() {
+		return (
+			<div>
+				<ProjectItem name="Newsletter Joiner" description="Junta todas as newsletters em um lugar só" />
+				<ProjectItem name="Newsletter Joiner" description="Junta todas as newsletters em um lugar só" />
+				<ProjectItem name="Newsletter Joiner" description="Junta todas as newsletters em um lugar só" />
+				<ProjectItem name="Newsletter Joiner" description="Junta todas as newsletters em um lugar só" />
+				<ProjectItem name="Newsletter Joiner" description="Junta todas as newsletters em um lugar só" />
+			</div>
+		)
+	}
+}
+
+export default ProjectList
